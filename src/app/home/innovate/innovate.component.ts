72d7e819deae4f29c17484b7502aec61748d1328
Tony <antoniodegollado10@gmail.com>
2019-07-06 17:02:34 -0500

    First commit

---
+++ src/app/home/innovate/innovate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-innovate',
+  templateUrl: './innovate.component.html',
+  styleUrls: ['./innovate.component.css']
+})
+export class InnovateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
