5b909e6b4f3ec973d61168001e1d56e67e66e6d5
Tony <antoniodegollado10@gmail.com>
2019-07-27 16:32:22 -0500

    Architecture section
    Added architecture section

---
+++ src/app/home/architecture-sec/architecture-sec.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-architecture-sec',
+  templateUrl: './architecture-sec.component.html',
+  styleUrls: ['./architecture-sec.component.css']
+})
+export class ArchitectureSecComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
