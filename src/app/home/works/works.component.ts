80e336064dad20dc7c5525d4232ac2b6357b3c55
Tony <antoniodegollado10@gmail.com>
2019-08-03 16:07:13 -0500

    Work Component
    Added work component

--- src/app/home/works/works.component.ts
+++ src/app/home/works/works.component.ts
@@ -11,5 +11,26 @@ export class WorksComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  works: Array<object> = [
+    {
+      work: 'Work name',
+      description: 'A super descriptive, description pendant to be written',
+      imgSrc: 'http://www.mgsarchitecture.in/images/MGS-Architecture/Architects/471-NU-DE-Architecture.jpg'
+    },
+    {
+      work: 'Work name',
+      description: 'A super descriptive, description pendant to be written',
+      imgSrc: 'http://www.mgsarchitecture.in/images/MGS-Architecture/Architects/471-NU-DE-Architecture.jpg'
+    },
+    {
+      work: 'Work name',
+      description: 'Place name',
+      imgSrc: 'http://www.mgsarchitecture.in/images/MGS-Architecture/Architects/471-NU-DE-Architecture.jpg'
+    },
+    {
+      work: 'Work name',
+      description: 'Place name',
+      imgSrc: 'http://www.mgsarchitecture.in/images/MGS-Architecture/Architects/471-NU-DE-Architecture.jpg'
+    }
+  ]
 }
